425cd0890d95fa5db636b1c478c0e889c020d892
Satya Aruna <satya.aruna@gmail.com>
2026-02-01 12:14:21 +0530

    files changed

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# color codes in Linux, can be enabled with echo -e option
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+
+USERID=$(id -u) # userid of root user 0, and others non-zero
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 # we need to exit with failure exit code
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# Functions are not executed by default, only executed when called
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing Mongodb"
+
+systemctl enable mongod
+VALIDATE $? "Enable Mongodb"
+
+systemctl start mongod
+VALIDATE $? "Start Mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "mongod.conf change to allow all connections"
+
+systemctl restart mongod
+VALIDATE $? "Restart Mongodb after config changes"
+
