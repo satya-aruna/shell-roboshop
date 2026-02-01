8dd800b52b029e1235163d153cbeca819b482306
Satya Aruna <satya.aruna@gmail.com>
2026-02-01 15:47:22 +0530

    files changed

---
+++ redis.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# color codes in Linux, can be enabled with echo -e option
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+
+USERID=$(id -u) # userid of root user 0, and others non-zero
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 # we need to exit with failure exit code
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# Functions are not executed by default, only executed when called
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y &>> $LOGS_FILE
+VALIDATE $? "Disabling Redis default version"
+
+dnf module enable redis:7 -y &>> $LOGS_FILE
+VALIDATE $? "Enabling Redis version 7"
+
+dnf install redis -y &>> $LOGS_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+VALIDATE $? "redis.conf change to allow all connections"
+
+sed -i 's/"protected-mode yes"/"protected-mode no"/g' /etc/redis/redis.conf
+VALIDATE $? "redis.conf change protected-mode to no"
+
+systemctl enable redis &>> $LOGS_FILE
+VALIDATE $? "Enable redis"
+
+systemctl start redis 
+VALIDATE $? "Start redis"
+
