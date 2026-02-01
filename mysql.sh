44fc0cbe31a8e03d7c1a865c7577ab906fb22a64
Satya Aruna <satya.aruna@gmail.com>
2026-02-01 16:58:57 +0530

    files changed

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# color codes in Linux, can be enabled with echo -e option
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+
+USERID=$(id -u) # userid of root user 0, and others non-zero
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 # we need to exit with failure exit code
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# Functions are not executed by default, only executed when called
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld &>> $LOGS_FILE
+VALIDATE $? "Enable mysql server"
+
+systemctl start mysqld 
+VALIDATE $? "Start mysql server"
