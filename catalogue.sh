8401f996e6c58f4fb1394a145e3cc039aed16171
Satya Aruna <satya.aruna@gmail.com>
2026-02-01 13:42:29 +0530

    files changed

--- catalogue.sh
+++ catalogue.sh
@@ -7,10 +7,11 @@ Y='\e[33m'
 B='\e[34m'
 N='\e[0m'
 
-SCRIPT_DIR=$(pwd)
+SCRIPT_DIR=$PWD # special variable for present working directory
 USERID=$(id -u) # userid of root user 0, and others non-zero
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+MONGODB_HOST="mongodb.asadaws2026.online"
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -65,17 +66,23 @@ VALIDATE $? "Unzip the application code"
 npm install  &>> $LOGS_FILE
 VALIDATE $? "Install dependencies"
 
-cd $SCRIPT_DIR
-VALIDATE $? "Moving back to Script directory after installing dependencies in /app dir"
-
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Setup Systemd catalogue service for systemctl"
 
 systemctl daemon-reload
 VALIDATE $? "Reload the newly created systemd catlogue service"
 
-systemctl enable catalogue
+systemctl enable catalogue &>> $LOGS_FILE
 VALIDATE $? "Enable catalogue service"
 
 systemctl start catalogue
 VALIDATE $? "Start catalogue service"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Setup mongodb repo"
+
+dnf install mongodb-mongosh -y &>> $LOGS_FILE
+VALIDATE $? "Installing mongodb client"
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOGS_FILE
+VALIDATE $? "Loading Master data into MongoDB"
